2bceab231fc7da11ee7042b38e2f052cf78372da
Daniel Santana Rocha <daniel.rocha@corp.globo.com>
2019-02-25 20:42:35 -0300

    Added Procfile for heroku and added --force for pushing for heroku git repo.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ git checkout -b heroku
 npm run-script build 
 git add dist/ -f
 git commit -m "Forcing commit on dist folder for heroku"
-git push heroku heroku
+git push heroku heroku --force
 git checkout master
 git branch -D heroku
