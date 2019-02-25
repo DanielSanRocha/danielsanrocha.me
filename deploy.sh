f37674ff9d42bd4b892d6daf4832ca567c40eceb
Daniel Santana Rocha <daniel.rocha@corp.globo.com>
2019-02-25 20:46:58 -0300

    Removed deploy.sh script, running build script in heroku instead.

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-git checkout -b heroku
-npm run-script build 
-git add dist/ -f
-git commit -m "Forcing commit on dist folder for heroku"
-git push heroku heroku --force
-git checkout master
-git branch -D heroku
