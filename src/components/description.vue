ba3d1f7082273c817efba6f1b12b6c5115cf79e4
Daniel Santana Rocha <daniel.rocha@corp.globo.com>
2019-02-25 23:29:30 -0300

    Added a description as the router index.

---
+++ src/components/description.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="description" class="container">
+    <h2 class="description">Mathematician, programmer and aspiring bassist.</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'description'
+}
+</script>
+
+<style @scoped>
+
+#description {
+    margin: 2% 5% 0 5%;
+}
+
+#description h2.description {
+    color: gray;
+    text-align: center;
+    font-size: 2.3em;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import description from '../components/description.vue'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'description',
+      component: description
+    }
   ]
 })
